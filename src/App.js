5d5dab9bbaacfa726ad8bbeae7b07d3533f19bab
khazadin <dervismehmetusta35@gmail.com>
2023-06-07 22:16:30 +0300

    ilk react app eklendi, state ve component'lar ogrenildi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+  const listMaker = () => {
+    setList([...list, task]);
+  };
+  const handleChange = (event) => {
+    setTask(event.target.value);
+  };
+  const deleteItem = (taskname) => {
+    const newToDo = list.filter((tasky) => {
+      if (tasky === taskname) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setList(newToDo);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>First React Project of Me</h1>
+      <h2>ToDo List</h2>
+      <div className="inpBut">
+        <input
+          className="inputBar"
+          type="text"
+          placeholder="What is your task?"
+          onChange={handleChange}
+        ></input>
+        <button className="buttonImp" onClick={listMaker}>
+          Add task
+        </button>
+      </div>
+      <div className="noteBook">
+        {list.map((tasky) => {
+          return (
+            <div className="taskBlock">
+              <div className="taskChild ">
+                <h3 style={{ marginRight: "20px" }}>{tasky}</h3>
+                <button
+                  className="finishButton"
+                  onClick={() => deleteItem(tasky)}
+                >
+                  Finished
+                </button>
+              </div>
+              <hr></hr>
+            </div>
+          );
+        })}
+      </div>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+      </style>
     </div>
   );
 }
